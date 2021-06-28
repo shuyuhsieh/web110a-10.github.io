6311c5ba9eb0c14915ee6b2701b9fcaba3424cbc
miwa <miwa.huang0625@gmail.com>
2021-06-29 01:22:38 +0800

    add order here

---
+++ foodfun/Areas/Staff/Controllers/StaffOrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foodfun.Areas.Staff.Controllers
+{
+    public class StaffOrderController : Controller
+    {
+
+        [LoginAuthorize(RoleList = "Admin/Staff")]
+        // GET: Staff/StaffOrder
+        public ActionResult Here()
+        {
+
+            return View(StaffOrder.GetOrderList("A"));
+        }
+    }
+}
