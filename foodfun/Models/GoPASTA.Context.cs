0c43b8b4737152c04cd229ab021cb5e1ba51ba3c
Doris Hsieh <shuyu.doris@gmail.com>
2021-07-05 11:51:19 +0800

    0705 part datepicker

--- foodfun/Models/GoPASTA.Context.cs
+++ foodfun/Models/GoPASTA.Context.cs
@@ -36,11 +36,11 @@ namespace foodfun.Models
         public virtual DbSet<Roles> Roles { get; set; }
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<Users> Users { get; set; }
-        public virtual DbSet<ProgramList> ProgramList { get; set; }
         public virtual DbSet<Orders> Orders { get; set; }
         public virtual DbSet<Products> Products { get; set; }
         public virtual DbSet<Company> Company { get; set; }
         public virtual DbSet<Concepts> Concepts { get; set; }
         public virtual DbSet<Profiles> Profiles { get; set; }
+        public virtual DbSet<ProgramList> ProgramList { get; set; }
     }
 }
